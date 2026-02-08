0a2bdbf556368fded201ef10df03ac49f6e430c5
Dmitry Polonnikov <d.polonnikoff@yandex.ru>
2026-02-08 13:35:32 +0300

    initial spring boot setup

---
+++ src/main/java/ru/polonnikov/smartdelivery/SmartdeliveryApplication.java
@@ -0,0 +1,13 @@
+package ru.polonnikov.smartdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/polonnikov/smartdelivery/SmartdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.polonnikov.smartdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
