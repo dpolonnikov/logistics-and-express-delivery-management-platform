8704b603127a7915f17efa83e7c18c33525d1558
Dmitry Polonnikov <d.polonnikoff@yandex.ru>
2026-02-11 11:49:38 +0300

    add restaurant model

---
+++ src/main/java/ru/polonnikov/smartdelivery/model/Restaurant.java
@@ -0,0 +1,28 @@
+package ru.polonnikov.smartdelivery.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "restaurants")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private String address;
+    private String description;
+    private Boolean isActive;
+}
